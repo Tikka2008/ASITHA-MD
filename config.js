39041a5e737f99d95aa84e4ebaa5d34c42591538
Tikka2008 <damsarat48@gmail.com>
2024-10-29 21:07:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "hucHQD6Y#jtXShKtf0pjo_tzykLhBR3pccd6bwD1_eDVA5ca-WBA",
+MONGODB: process.env.MONGODB || "mongodb://mongo:poWHMYJyJnQRVIObPxLITSKyebEUdyMF@junction.proxy.rlwy.net:54546",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
